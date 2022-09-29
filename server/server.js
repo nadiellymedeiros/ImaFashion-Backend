bbe0799fe88f515f2b92b73c582e33fb0b999669
nadiellymedeiros <nadiellymedeiros@gmail.com>
2022-09-28 23:23:06 -0300

    inclusão do dotenv

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const server = express();
+require("dotenv").config();
 const cors = require("cors");
 const port = 80;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
 
 server.use(cors());
 
